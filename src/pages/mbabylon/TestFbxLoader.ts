96fe8285fdbab9cf995c991a16dc5aacdc8c9878
ztl007@dingtalk.com <ztl007@dingtalk.com>
2024-11-04 19:28:29 +0800

    work

--- src/pages/mbabylon/TestFbxLoader.ts
+++ src/pages/mbabylon/TestFbxLoader.ts
@@ -1,12 +1,20 @@
 import {
     SceneLoader, ArcRotateCamera, Vector3, DirectionalLight,
-} from "babylonjs"
-
-// import '@babylonjs/loaders';
+    InitializeCSG2Async,
+    MeshBuilder,
+    CSG2,
+    VertexBuffer,
+    CSG,
+    Mesh,
+    Matrix,
+    Color3
+} from "@babylonjs/core"
+// import 'babylonjs-loaders';
 import { FBXLoader } from "../../lib/babylonjs-fbx-loader-master"
 // @ts-ignore
 SceneLoader.RegisterPlugin(new FBXLoader())
 import { CreateScene } from '../../lib/babylon-helper'
+
 
 export async function TestFbxLoader(CanvasEl: HTMLCanvasElement) {
     const scene = CreateScene(CanvasEl)
@@ -24,30 +32,181 @@ export async function TestFbxLoader(CanvasEl: HTMLCanvasElement) {
     // 允许用户通过鼠标控制相机
     camera.attachControl(CanvasEl, true);
 
-
     // 添加光源到场景
     let light = new DirectionalLight("DirectionalLight",
         new Vector3(-1, -1, -1),
         scene);
-    light.intensity = 1;  //调整平行光的强度
-    // let light1 = new BABYLON.HemisphericLight("light1",
-    //     new BABYLON.Vector3(1, 1, 0),
-    //     scene);
-    // let light2 = new BABYLON.PointLight("light2",
-    //     new BABYLON.Vector3(0, 1, -1),
-    //     scene);
+    light.intensity = 5;  //调整平行光的强度
+
+
+
+    // TODO
+    await InitializeCSG2Async();
+
+
+    // Our built-in 'sphere' shape.
+    // var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    // sphere.useVertexColors = true;
+    // // Move the sphere upward 1/2 its height
+    // sphere.position.y = 1;
+    // let sphereVertCount = sphere.getTotalVertices();
+    // let sphereColors = []
+    // for(let i=0; i< sphereVertCount; i++){
+    //     sphereColors.push(0,1,0,1);
+    // }
+    // sphere.setVerticesData(VertexBuffer.ColorKind, sphereColors);
+
+    var box = MeshBuilder.CreateBox("box", { size: 10 }, scene);
+    box.position = new Vector3(17, 35, 6);
+    box.useVertexColors = true;
+    let boxVertCount = box.getTotalVertices();
+    let boxColors = []
+    for (let i = 0; i < boxVertCount; i++) {
+        boxColors.push(1, 1, 1, 1);
+    }
+    box.setVerticesData(VertexBuffer.ColorKind, boxColors);
+
+
+    // let boxCSG = CSG.FromMesh(box);
+    // let booleanCSG = sphereCSG.subtract(boxCSG);
+    // let newMesh = booleanCSG.toMesh("newMesh", null, scene,true);
+    // sphere.visibility = 0;
+    // box.visibility = 0;
+
+
 
 
     SceneLoader.ImportMesh(
         '', // 要导入的特定网格的名称，空字符串表示导入所有网格
         "/static/mbabylon/models/animation/",// 模型文件的路径
-        "Defeated.fbx", // 模型文件的名称
+        "house.fbx", // 模型文件的名称
         scene, // 要将模型导入到的目标场景
         function (meshes) { // 回调函数，处理加载完成后的操作
             // meshs是模型中的所有网格，是模型的基本组成后续要实现各种交互需要了解。
-            // 一会的鼠标监听事件就写在这里
-            console.log('模型已加载', meshes);
+            meshes.forEach(mesh => {
+                if (mesh.name == "__root__")
+                    return
+                let position = mesh.position
+                // position.x+=1
+                position.x = position.x / 100
+                position.y = position.y / 100
+                position.z = position.z / 100
+                let scaling = mesh.scaling
+                scaling.x = scaling.x / 100
+                scaling.y = scaling.y / 100
+                scaling.z = scaling.z / 100
+            })
+
+            // let position = meshes[0].position
+            // position.x+=1
+            // position.x = position.x / 100
+            // position.y = position.y / 100
+            // position.z = position.z / 100
+            // let scaling = meshes[0].scaling
+            // scaling.x = scaling.x / 100
+            // scaling.y = scaling.y / 100
+            // scaling.z = scaling.z / 100
+
+
+            let rootMesh = new Mesh("__root__1", scene)
+            meshes.forEach(mesh => {
+                if (!(mesh instanceof Mesh) || mesh.name == "__root__")
+                    return
+                //@ts-ignore
+
+                // let position = Vector3.TransformCoordinates(mesh.position, meshes[0].getWorldMatrix());
+                // let scaling = Vector3.TransformCoordinates(mesh.scaling, meshes[0].getWorldMatrix());
+                
+                const intersects1 = mesh.getBoundingInfo().boundingBox.intersectsMinMax(box.getBoundingInfo().boundingBox.minimum,box.getBoundingInfo().boundingBox.maximum);
+                const intersects2 = box.getBoundingInfo().boundingBox.intersectsMinMax(mesh.getBoundingInfo().boundingBox.minimum,mesh.getBoundingInfo().boundingBox.maximum);
+                if (!intersects1 && !intersects2) {
+                    let news = mesh.clone()
+                    news.parent = rootMesh
+                    news.position.z = 150
+                    return
+                }
+
+                let meshCSG = CSG.FromMesh(mesh);
+                let boxCSG = CSG.FromMesh(box);
+
+                let booleanCSG = meshCSG.subtract(boxCSG);
+                let newMesh = booleanCSG.toMesh(mesh.name + "1", mesh.material, scene, true);
+                let subMeshes1 = mesh.subMeshes
+                subMeshes1.forEach((subMesh) => {
+                    console.log(subMesh.getMaterial());
+                })
+                console.log(123);
+                let subMeshes2 = newMesh.subMeshes
+                subMeshes2.forEach((subMesh) => {
+                    console.log(subMesh.getMaterial());
+                })
+
+
+                let subMaterial1 = mesh.material?.subMaterials!
+                let subMaterial2 = newMesh.material?.subMaterials!
+                // newMesh.material = mesh.material
+
+
+
+                // newMesh.updateFacetData();
+                // var positions = newMesh.getFacetLocalPositions();
+                // var normals = newMesh.getFacetLocalNormals();
+                // var lines = [];
+                // for (var i = 0; i < positions.length; i++) {
+                //     var line = [positions[i], positions[i].add(normals[i])];
+                //     lines.push(line);
+                // }
+                // var lineSystem = MeshBuilder.CreateLineSystem("ls", { lines: lines }, scene);
+                // lineSystem.color = Color3.Blue();
+
+
+
+
+                newMesh.parent = rootMesh
+                // newMesh.position = position;
+                // newMesh.scaling = scaling;
+                newMesh.position.z = 150
+                // mesh.visibility = 0;
+            })
         }
-        
     )
+    return scene;
 }
+
+// function isT(mesh1: Mesh, mesh2: Mesh) {
+//     let box1 = mesh1.getBoundingInfo().boundingBox
+//     let box1W = box1.vectors.map((local) => {
+//         return Vector3.TransformCoordinates(local, box1.getWorldMatrix());
+//     })
+//     let box12d=box1W.forEach()
+//     let box2 = mesh2.getBoundingInfo().boundingBox
+//     let box2W = box2.vectors.map((local) => {
+//         return Vector3.TransformCoordinates(local, box1.getWorldMatrix());
+//     })
+//     for (const v of box1W) {
+//         const flag = pointInPolygon({
+//             x: v.x,
+//             y: v.x,
+//         }, points);
+//         if (flag) return true;
+//     }
+//     for (const v of boundingBoxPoints) {
+//         const flag = pointInPolygon({
+//             x: v.x,
+//             x: v.x,
+//         }, points);
+//         if (flag) return true;
+//     }
+// }
+
+// export type Point2D = { x: number, y: number };
+// export type Line2D = [Point2D, Point2D];
+// export type Polygon2D = Point2D[];
+// export const isPointInPolygon = (point: Point2D, polygon: Polygon2D) => {
+//     let inRange = false;
+//     for (let i = -1, l = polygon.length, j = l - 1; ++i < l; j = i)
+//       ((polygon[i].y <= point.y && point.y < polygon[j].y) || (polygon[j].y <= point.y && point.y < polygon[i].y))
+//         && (point.x < (polygon[j].x - polygon[i].x) * (point.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x)
+//         && (inRange = !inRange);
+//     return inRange;
+//   };
