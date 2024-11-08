558b1d91462b6c838a633230996c88cb0bd757f4
ztl007@dingtalk.com <ztl007@dingtalk.com>
2024-11-08 18:26:12 +0800

    csg.cpp

--- src/pages/mbabylon/TestFbxLoader.ts
+++ src/pages/mbabylon/TestFbxLoader.ts
@@ -29,7 +29,7 @@ import { CreateScene } from '../../lib/babylon-helper';
 
 import initOpenCascade, { gp_Pnt, TopoDS_Shape } from "./opencascade.js/dist/index.js";
 import { verticesData1 } from "./data.js";
-
+import * as CG from "./emcc.js"
 export async function TestFbxLoader(CanvasEl: HTMLCanvasElement) {
     const scene = CreateScene(CanvasEl)
 
@@ -51,145 +51,25 @@ export async function TestFbxLoader(CanvasEl: HTMLCanvasElement) {
         new Vector3(-1, -1, -1),
         scene);
     light.intensity = 5;  //调整平行光的强度
-
-
-    let oc = await initOpenCascade()
-    let positions = verticesData1.positions
-    let indices = verticesData1.indices
-    // 创建顶点
-    function createPoint(x:number, y:number, z:number) {
-        return new oc.gp_Pnt_3(x, y, z); // gp_Pnt 用来表示顶点
-    }
-    // 创建边
-    function createEdge(p1: gp_Pnt, p2: gp_Pnt) {
-        let tt=new oc.BRepBuilderAPI_MakeEdge_3(p1, p2).Edge();
-        return tt; // BRepBuilderAPI_MakeEdge 用来生成边
-    }
-    // 创建一个面
-    function createFaceFromPositionsAndIndices(positions:number[], indices:number[]) {
-        let wireBuilder = new oc.BRepBuilderAPI_MakeWire_1();
-        
-        // 创建边并将其添加到 Wire
-        for (let i = 0; i < 3; i += 3) {
-            // 一个三角形
-            let idx = [indices[i], indices[i + 1], indices[i + 2]]
-            // 一个三角形的三个顶点
-            let pos = [
-                [positions[idx[0]*3],positions[idx[0]*3+1],positions[idx[0]*3+2]],
-                [positions[idx[1]*3],positions[idx[1]*3+1],positions[idx[1]*3+2]],
-                [positions[idx[2]*3],positions[idx[2]*3+1],positions[idx[2]*3+2]],
-            ]
-            let p1 = createPoint(pos[0][0],pos[0][1],pos[0][2]);
-            let p2 = createPoint(pos[1][0],pos[1][1],pos[1][2]);
-            let p3 = createPoint(pos[2][0],pos[2][1],pos[2][2]);
-
-            wireBuilder.Add_1(createEdge(p1, p2));
-            wireBuilder.Add_1(createEdge(p1, p3));
-            wireBuilder.Add_1(createEdge(p2, p3));
-        }
-        if (!wireBuilder.IsDone()) {
-            console.log(123);
-        }
-        // 用 Wire 构造面
-        let wire = wireBuilder.Wire();
-        let faceBuilder = new oc.BRepBuilderAPI_MakeFace_1();
-        faceBuilder.Add(wire)
-        
-        return faceBuilder.Face(); // 返回创建的面
-    }
-    // 创建几何体（例如一个由三角形组成的网格体）
-    let shape1 = createFaceFromPositionsAndIndices(positions, indices);
-
-//     ShapeFix_Shape fixer(shape);
-// fixer.Perform();  // 修复几何体
-
-
-    // 使用 BRepAlgoAPI_Cut 执行布尔减法
-    let cut = new oc.BRepAlgoAPI_Cut_3(shape1, shape1,new oc.Message_ProgressRange_1());
-    cut.Build(new oc.Message_ProgressRange_1());
-    // 获取布尔运算结果
-    let resultShape = cut.Shape();
-
-
-
-
-
-
-    // const sphere = new oc.BRepPrimAPI_MakeSphere_1(1);
-    // // Take shape and subtract a translated and scaled sphere from it
-    // const makeCut = (shape: TopoDS_Shape, translation: [number, number, number], scale: number) => {
-    //     const tf = new oc.gp_Trsf_1();
-    //     tf.SetTranslation_1(new oc.gp_Vec_4(translation[0], translation[1], translation[2]));
-    //     tf.SetScaleFactor(scale);
-    //     const loc = new oc.TopLoc_Location_2(tf);
     
-    //     const cut = new oc.BRepAlgoAPI_Cut_3(shape, sphere.Shape().Moved(loc, false), new oc.Message_ProgressRange_1());
-    //     cut.Build(new oc.Message_ProgressRange_1());
-    
-    //     return cut.Shape();
-    // };
-    
-    // // Let's make some cuts
-    // const cut1 = makeCut(sphere.Shape(), [0, 0, 0.7], 1);
-    // const cut2 = makeCut(cut1, [0, 0, -0.7], 1);
-    // const cut3 = makeCut(cut2, [0, 0.25, 1.75], 1.825);
-    // const cut4 = makeCut(cut3, [4.8, 0, 0], 5);
-    
-    // // Rotate around the Z axis
-    // const makeRotation = (rotation: number) => {
-    //     const tf = new oc.gp_Trsf_1();
-    //     tf.SetRotation_1(new oc.gp_Ax1_2(new oc.gp_Pnt_1(), new oc.gp_Dir_4(0, 0, 1)), rotation);
-    //     const loc = new oc.TopLoc_Location_2(tf);
-    //     return loc;
-    // };
-    
-    // // Combine the result
-    // const fuse = new oc.BRepAlgoAPI_Fuse_3(cut4, cut4.Moved(makeRotation(Math.PI), false), new oc.Message_ProgressRange_1());
-    // fuse.Build(new oc.Message_ProgressRange_1());
-    // const result = fuse.Shape().Moved(makeRotation(-30 * Math.PI / 180), false);
+  
+    console.log();
+  
 
 
-    
-
-
-
-
-
-    
-    // // Make 2 meshes..
-    // let geometry1 = new THREE.BufferGeometry();
-    
-    // let positions = new Float32Array(verticesData1.positions);
-    // geometry1.attributes.position = new THREE.BufferAttribute(positions, 3);
- 
-    // let indices = new Uint16Array(verticesData1.indices);
-    // geometry1.index = new THREE.BufferAttribute(indices, 1);
-    // let n:number[] = []
-    // for (let i = 0; i < positions.length;i+=3){
-    //     n.push(0,0,1)
-    // }
-    // let normals=new Uint16Array(n)
-    // geometry1.setAttribute('normal',new THREE.BufferAttribute(normals, 3));
-
-
-    // const house = new THREE.Mesh(geometry1,new THREE.MeshNormalMaterial());
-    // house.updateMatrix();
-
-    // const box = new THREE.Mesh(
-    //     new THREE.BoxGeometry(2, 2, 2),
-    //     new THREE.MeshNormalMaterial()
-    // );
-    // // Make sure the .matrix of each mesh is current
-    // box.updateMatrix();
-
-    // // Perform CSG operations
-    // // The result is a THREE.Mesh that you can add to your scene...
-    // console.time('计时器');
-    // const subRes = threeCGS.subtract(house, box);
-    // // const uniRes = threeCGS.union(box, sphere);
-    // // const intRes = threeCGS.intersect(box, sphere);
-    // // 结束计时
-    // console.timeEnd('计时器');
+    // WebAssembly.instantiateStreaming(fetch("/static/hello-emcc.wasm"), {
+    //     imports: {
+    //         imported_func: function(arg:any) {
+    //           console.log(arg);
+    //         },
+    //         wasi_unstable: () => {}
+    //       }
+    // }).then(
+    //     (results) => {
+    //       console.log(results);
+          
+    //     },
+    //   );
 
     
 
@@ -303,39 +183,39 @@ export async function TestFbxLoader(CanvasEl: HTMLCanvasElement) {
 
 
   
-// Convert Manifold Mesh to  
-function mesh2vertexData(mesh: Manifold3dMeshType):VertexData {
-    const vertexData = new VertexData()
-    // Assign buffers
-    vertexData.positions = mesh.vertProperties
-    vertexData.indices=mesh.triVerts
-    return vertexData;
-}
+// // Convert Manifold Mesh to  
+// function mesh2vertexData(mesh: Manifold3dMeshType):VertexData {
+//     const vertexData = new VertexData()
+//     // Assign buffers
+//     vertexData.positions = mesh.vertProperties
+//     vertexData.indices=mesh.triVerts
+//     return vertexData;
+// }
 
-// Convert   to Manifold Mesh
-function vertexData2mesh(vertexData:VertexData): Manifold3dMeshType{
-    // Only using position in this sample for simplicity. Can interleave any other
-    // desired attributes here such as UV, normal, etc.
+// // Convert   to Manifold Mesh
+// function vertexData2mesh(vertexData:VertexData): Manifold3dMeshType{
+//     // Only using position in this sample for simplicity. Can interleave any other
+//     // desired attributes here such as UV, normal, etc.
 
-    // positions
-    const positions = vertexData.positions;
-    // UV
-    // normal
-    // Manifold only uses indexed geometry, so generate an index if necessary.
-    const indices = vertexData.indices;
+//     // positions
+//     const positions = vertexData.positions;
+//     // UV
+//     // normal
+//     // Manifold only uses indexed geometry, so generate an index if necessary.
+//     const indices = vertexData.indices;
 
-    // Create the MeshGL for I/O with Manifold library.
+//     // Create the MeshGL for I/O with Manifold library.
 
-    const mesh =new Manifold3dMesh({vertProperties:positions, triVerts:indices});
+//     const mesh =new Manifold3dMesh({vertProperties:positions, triVerts:indices});
         
-    // Automatically merge vertices with nearly identical positions to create a
-    // Manifold. This only fills in the mergeFromVert and mergeToVert vectors -
-    // these are automatically filled in for any mesh returned by Manifold. These
-    // are necessary because GL drivers require duplicate verts when any
-    // properties change, e.g. a UV boundary or sharp corner.
-    mesh.merge();
-    return mesh;
-}
+//     // Automatically merge vertices with nearly identical positions to create a
+//     // Manifold. This only fills in the mergeFromVert and mergeToVert vectors -
+//     // these are automatically filled in for any mesh returned by Manifold. These
+//     // are necessary because GL drivers require duplicate verts when any
+//     // properties change, e.g. a UV boundary or sharp corner.
+//     mesh.merge();
+//     return mesh;
+// }
 
 
 
