3a4e67ffe44121e3bc939053fa550639df0f6c3c
ztl007@dingtalk.com <ztl007@dingtalk.com>
2024-11-11 09:07:40 +0800

    csgcpp

--- static/temp/csgjs-cpp-master-2/checkimpl.cpp
+++
@@ -1,8 +0,0 @@
-//this file is only here to make sure that the header
-//csgjs behaves well as an implementation (that only
-//appears in one translation unit) and a header that
-//will presumably appear in many.
-
-
-#define CSGJSCPP_IMPLEMENTATION
-#include "mycsgjs.h"
